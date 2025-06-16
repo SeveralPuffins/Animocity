96d7e4ce961d24f836240d0e835d1fa1e55fa6f0
EffieArtoria <effieartoria@gmail.com>
2025-06-16 18:36:10 +0100

    Add battle scene with very rudimentary movement controls and two mechs.

---
+++ Animocity/Assets/Scenes/BattleScene/Scripts/BattleMechInputController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Scenes.BattleScene.Scripts
+{
+    public class BattleMechInputController : MonoBehaviour
+    {
+        private BattleControls _controls;
+        [SerializeField] private BattleMechMovementController _movementController;
+
+        private void Awake()
+        {
+            _controls = new BattleControls();
+        }
+
+        private void OnEnable()
+        {
+            _controls.Enable();
+            _controls.BattleActions.Movement.performed += MovementOnPerformed;
+            _controls.BattleActions.Movement.canceled += MovementOnCancel;
+        }
+        private void OnDisable()
+        {
+            _controls.BattleActions.Movement.performed -= MovementOnPerformed;
+            _controls.BattleActions.Movement.canceled -= MovementOnCancel;
+        }
+
+        private void MovementOnCancel(InputAction.CallbackContext context)
+        {
+
+            _movementController.StopMoving();
+        }
+
+        private void MovementOnPerformed(InputAction.CallbackContext context)
+        {
+            _movementController.Move(context.ReadValue<Vector2>());
+        }
+    }
+}
---
+++ Animocity/Assets/Scenes/BattleScene/Scripts/BattleMechMovementController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace Scenes.BattleScene
+{
+    public class BattleMechMovementController : MonoBehaviour
+    {
+        [SerializeField] Rigidbody rigidbody;
+        [SerializeField] float moveSpeed;
+        private Vector3 _movement;
+        
+        public void Move(Vector2 direction)
+        {
+            // direction.
+            _movement = new Vector3(direction.x, 0, 0);
+        }
+
+        public void StopMoving()
+        {
+            _movement = Vector2.zero;
+        }
+
+        private void Update()
+        {
+            rigidbody.MovePosition(rigidbody.position + _movement * (moveSpeed * Time.fixedDeltaTime));
+        }
+    }
+}
